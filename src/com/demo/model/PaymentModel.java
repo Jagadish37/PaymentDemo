02ae506e4fd0072936cebaacb5ff280c259532fc
Jagadish37 <30297012+Jagadish37@users.noreply.github.com>
2018-04-24 13:56:45 +0200

    model class added

---
+++ src/com/demo/model/PaymentModel.java
@@ -0,0 +1,49 @@
+package com.demo.model;
+
+import java.io.Serializable;
+
+public class PaymentModel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4661864829191447679L;
+	private String date;
+	private String details;
+	private String amount;
+	private String status;
+	
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+}
