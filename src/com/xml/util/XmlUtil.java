ddfd207cb5d66b57135d89a713b6ac93a9ee7425
Jagadish37 <30297012+Jagadish37@users.noreply.github.com>
2018-04-24 13:57:37 +0200

    added xmlutil

---
+++ src/com/xml/util/XmlUtil.java
@@ -0,0 +1,109 @@
+package com.xml.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import com.demo.model.PaymentModel;
+
+public class XmlUtil {
+
+	public List<String> parseXML(String name, HashMap<String, String> hm) {
+
+		SAXBuilder builder = new SAXBuilder();
+		ClassLoader classLoader = getClass().getClassLoader();
+		File xmlfile = new File(classLoader.getResource("\\main\\resources\\Payment.xml").getFile());
+		List<String> listofPayments = new ArrayList<>();
+
+		try {
+
+			Document document = (Document) builder.build(xmlfile);
+			Element rootNode = document.getRootElement();
+			/*List list = rootNode.getChildren("entry");
+
+			for (int i = 0; i < list.size(); i++) {
+				Element node = (Element) list.get(i);
+
+				System.out.println(node.getChildText("Details"));
+			}
+*/
+			if (name.equalsIgnoreCase("confirmed")) {
+				Element child = new Element("entry");
+				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+				Date date = new Date();
+				
+				hm.forEach((k, v) -> {
+					child.addContent(new Element(k).setText(v));
+				});
+				child.addContent(new Element("Date").setText(dateFormat.format(date)));
+				rootNode.addContent(child);
+
+				XMLOutputter xmlOutput = new XMLOutputter();
+
+				// display nice nice
+				xmlOutput.setFormat(Format.getPrettyFormat());
+				xmlOutput.output(document, new FileWriter(xmlfile));
+
+				// xmlOutput.output(doc, System.out);
+
+				System.out.println("File updated!");
+			}
+
+		}
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return listofPayments;
+	}
+
+	public List<PaymentModel> readxml (){
+		
+		
+		SAXBuilder builder = new SAXBuilder();
+		ClassLoader classLoader = getClass().getClassLoader();
+		File xmlfile = new File(classLoader.getResource("\\main\\resources\\Payment.xml").getFile());
+		List<PaymentModel> listofPayments = new ArrayList<>();
+
+		try {
+
+			Document document = (Document) builder.build(xmlfile);
+			Element rootNode = document.getRootElement();
+			List list = rootNode.getChildren("entry");
+			PaymentModel pm = null;
+			for (int i = 0; i < list.size(); i++) {
+				Element node = (Element) list.get(i);
+				pm= new PaymentModel();
+				pm.setAmount(node.getChildText("Amount"));
+				pm.setDetails(node.getChildText("remarks"));
+				pm.setDate(node.getChildText("Date"));
+				pm.setStatus("Completed");
+				listofPayments.add(pm);
+			}
+
+			
+		}
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listofPayments;
+		
+	}
+
+}
