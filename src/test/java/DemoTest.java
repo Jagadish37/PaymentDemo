93915ea1717e145f479067450156db017e57ee04
Jagadish37 <30297012+Jagadish37@users.noreply.github.com>
2018-04-24 13:59:07 +0200

    sample Test class added

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+
+public class DemoTest {
+
+	@Test
+	public void test() {
+		String str = "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	      
+	}
+
+	
+	
+}
