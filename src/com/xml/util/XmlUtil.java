b81d42f59ca1d116622fc9fe1553afef03a0d4ee
Jagadish37 <30297012+Jagadish37@users.noreply.github.com>
2018-06-11 15:31:18 +0200

    Update XmlUtil.java

--- src/com/xml/util/XmlUtil.java
+++ src/com/xml/util/XmlUtil.java
@@ -24,8 +24,8 @@ public class XmlUtil {
 	
 	SAXBuilder builder = new SAXBuilder();
 	ClassLoader classLoader = getClass().getClassLoader();
-	File xmlfile = new File(classLoader.getResource("\\main\\resources\\Payment.xml").getFile());
-
+	File xmlfile = new File("D:\\Projects\\Budapest\\Payment.xml");
+	
 	public List<String> parseXML(String name, HashMap<String, String> hm) {
 
 		
@@ -36,36 +36,26 @@ public class XmlUtil {
 
 			Document document = (Document) builder.build(xmlfile);
 			Element rootNode = document.getRootElement();
-			/*List list = rootNode.getChildren("entry");
-
-			for (int i = 0; i < list.size(); i++) {
-				Element node = (Element) list.get(i);
-
-				System.out.println(node.getChildText("Details"));
-			}
-*/
-			if (name.equalsIgnoreCase("confirmed")) {
+			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		
 				Element child = new Element("entry");
-				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-				Date date = new Date();
 				
+				if (name.equalsIgnoreCase("favorite")) 
+					child.setAttribute("favorite", "true");
+				
+				Date date = new Date();
 				hm.forEach((k, v) -> {
 					child.addContent(new Element(k).setText(v));
 				});
 				child.addContent(new Element("Date").setText(dateFormat.format(date)));
 				rootNode.addContent(child);
-
 				XMLOutputter xmlOutput = new XMLOutputter();
-
 				// display nice nice
 				xmlOutput.setFormat(Format.getPrettyFormat());
 				xmlOutput.output(document, new FileWriter(xmlfile));
-
 				// xmlOutput.output(doc, System.out);
 
 				System.out.println("File updated!");
-			}
-
 		}
 
 		catch (Exception e) {
@@ -86,7 +76,7 @@ public class XmlUtil {
 			Element rootNode = document.getRootElement();
 			mainBal=rootNode.getChildText("mainbalance");
 			
-			
+			System.out.println("Printing MainBal" + mainBal);
 			
 		} catch (JDOMException e) {
 			// TODO Auto-generated catch block
@@ -102,10 +92,42 @@ public class XmlUtil {
 		return mainBal;
 	}
 	
+	
+	
+public void setMainbalance(String balance){
+		
+		
+		Document document;
+		try {
+			document = (Document) builder.build(xmlfile);
+			Element rootNode = document.getRootElement();
+			rootNode.getChild("mainbalance").setText(balance);
+			
+			XMLOutputter xmlOutput = new XMLOutputter();
+
+			// display nice nice
+			xmlOutput.setFormat(Format.getPrettyFormat());
+			xmlOutput.output(document, new FileWriter(xmlfile));
+
+			// xmlOutput.output(doc, System.out);
+
+			System.out.println("File updated!");
+			
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	public List<PaymentModel> readxml (){
 		
 		List<PaymentModel> listofPayments = new ArrayList<>();
-
+		System.out.println(xmlfile);
 		try {
 
 			Document document = (Document) builder.build(xmlfile);
@@ -119,6 +141,7 @@ public class XmlUtil {
 				pm.setDetails(node.getChildText("remarks"));
 				pm.setDate(node.getChildText("Date"));
 				pm.setStatus("Completed");
+				pm.setBenName(node.getChildText("bName"));
 				listofPayments.add(pm);
 			}
 
@@ -133,5 +156,51 @@ public class XmlUtil {
 		return listofPayments;
 		
 	}
+	
+	
+	
+public List<PaymentModel> getFavorites (){
+		
+		List<PaymentModel> listofPayments = new ArrayList<>();
+		System.out.println(xmlfile);
+		try {
 
+			Document document = (Document) builder.build(xmlfile);
+			Element rootNode = document.getRootElement();
+			List list = rootNode.getChildren("entry");
+			PaymentModel pm = null;
+			boolean favCheck ;
+			for (int i = 0; i < list.size(); i++) {
+				Element node = (Element) list.get(i);
+				 favCheck = node.hasAttributes();
+				if(favCheck){
+					pm= new PaymentModel();
+					pm.setAmount(node.getChildText("Amount"));
+					pm.setDetails(node.getChildText("remarks"));
+					pm.setDate(node.getChildText("Date"));
+					pm.setBenName(node.getChildText("bName"));
+					listofPayments.add(pm);
+				}
+				
+				
+			}
+
+			
+		}
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listofPayments;
+		
+	}
+	
+	public static void main (String[] args){
+		
+		XmlUtil x= new XmlUtil();
+		
+		x.getFavorites();
+	}
 }
